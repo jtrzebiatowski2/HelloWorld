813b6d1c8c22c2e18b9535ea11879e3df34e1286
J-Tron <J-Tron@J-Tron-PC>
2013-08-26 17:55:08 -0500

    Added "Hello World" to print to screen

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
     }
    
 }
